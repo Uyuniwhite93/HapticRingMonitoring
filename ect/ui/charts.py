ace6a421233197ce6c4d1f1d3be8090d31fa9fd4
JHKim_ETRI <jaehun.kim@etri.re.kr>
2025-06-04 10:21:16 +0900

    d

--- ect/ui/charts.py
+++
@@ -1,7 +0,0 @@
-from PyQt6.QtCharts import QChart, QChartView, QLineSeries, QValueAxis
-from PyQt6.QtGui import QPainter, QColor, QPen, QBrush, QLinearGradient, QPainterPath, QPixmap, QRadialGradient, QFont, QFontDatabase, QCursor
-
-class ChartView(QChartView):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setRenderHint(QPainter.RenderHint.Antialiasing)
